da330d7e41175c6e2d0f1bc9e8727cc3ef5694ba
Niklas Broge <nihabr@gmail.com>
2017-04-04 14:26:17 +0200

    Updating repo

--- csvTutorial.py
+++ csvTutorial.py
@@ -2,6 +2,8 @@
 import csv
 import sys
 import itertools as iterT
+from nltk.corpus import stopwords
+from nltk.tokenize import wordpunct_tokenize
 
 # Define some variables we will need for later
 # Variables that are declared inside a function will not be usable outside of
@@ -13,9 +15,9 @@ trainData = ''
 
 # Create a function called main. In order to run the code in this script
 # We will call on the main() function in the bottom of the code.
-def printRows (filename, firstrowID = None, lastrowID = None):
+def printRows (data, firstrowID = None, lastrowID = None):
     """
-    Parameters: (firstrowID = None, lastrowID = None)
+    Parameters: (list data, int firstrowID = None, int lastrowID = None)
 
     Description: Prints rowID, Question 1, Question 2, and is_dublicate from
     train.csv in a reader friendly manner. Will start from firstrowID and return
@@ -54,8 +56,6 @@ def printRows (filename, firstrowID = None, lastrowID = None):
     # provide code heirarchy. All code on this indentation level
     # will pertain to this function.
 
-    data = readCSVfile(filename)
-
     # We want to save the first row of the data as a header, so that
     # we can use this for any models we may want to build. Typically,
     # the first row of csv files will descripe the setup of the csv rows.
@@ -67,7 +67,6 @@ def printRows (filename, firstrowID = None, lastrowID = None):
         lastrowID = firstrowID + 101
     else:
         lastrowID += 1
-
 
     # By using a for loop we can iterate over every row in the reader object
     # And define actions that should be done them.
@@ -115,4 +114,68 @@ def readCSVfile (filename):
         data = list(trainReader)
 
         trainOpen.close()
+
     return data
+
+def writeCSVfile (file, filename, path = ''):
+    '''
+    Parameters: file list, filename str
+    file will be written to the specified path, with the filename
+    '''
+    with open (path + filename, 'w', encoding='utf8', newline='') as newfile:
+        writer = csv.writer(newfile, delimiter = ',')
+        writer.writerows(file)
+
+        newfile.close()
+    print('File ' + filename + ' has been written to path: ' + 'C:/Users/nihab/workspace/itProject/' + path)
+
+def removeStopwords (data):
+    stop = set(stopwords.words('english'))
+    # STILL NEED TO APPLY THIS
+    stop.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}'])
+    for row in data:
+        # Treat question 1
+        temp = [word.lower() for word in wordpunct_tokenize(row[3]) if word.lower() not in stop]
+        row[3] = ' '.join(temp)
+        # Treat question 2
+        temp = [word.lower() for word in wordpunct_tokenize(row[4]) if word.lower() not in stop]
+        row[4] = ' '.join(temp)
+
+    return data
+
+# Source: https://www.kaggle.com/philschmidt/quora-question-pairs/quora-interactive-eda-model-selection
+def normalized_word_share(row):
+    '''
+    Comment by Niklas:
+    This function is rather complicated - as such I have provided explanation to help understand what happens:
+        set() = "A set is an unordered collection of items. Every element is unique (no duplicates) and must be immutable (which cannot be changed)."
+         Source: https://www.programiz.com/python-programming/set
+        map() = "Map applies a function to all the items in an input_list." Blueprint: map(function_to_apply, list_of_inputs). Source / documentation: http://book.pythontips.com/en/latest/map_filter.html
+        lambda = imbedded function. Like def (function name):, but not global?
+        lower(): sets all character to lowercase
+        strip(): removes whitespace / punctuation from words
+        split(): splits a string into substrings, in this case using space as a splitter. This means the string is seperated into words since all words are seperated by space.
+        len(): returns the length of input
+    '''
+    w1 = set(map(lambda word: word.lower().strip(), row['question1'].split(" ")))
+    w2 = set(map(lambda word: word.lower().strip(), row['question2'].split(" ")))
+    print('len(w1): ' + str(len(w1)))
+    print('len(w2): ' + str(len(w2)))
+    print('len(w1 & w2): ' + str(len(w1 & w2)))
+
+    return 1.0 * len(w1 & w2)/(len(w1) + len(w2))
+
+def word_match_share(row):
+    q1words = {}
+    q2words = {}
+    for word in str(row['question1']).lower().split():
+        q1words[word] = 1
+    for word in str(row['question2']).lower().split():
+        q2words[word] = 1
+    if len(q1words) == 0 or len(q2words) == 0:
+        # The computer-generated chaff includes a few questions that are nothing but stopwords
+        return 0
+    shared_words_in_q1 = [w for w in q1words.keys() if w in q2words]
+    shared_words_in_q2 = [w for w in q2words.keys() if w in q1words]
+    R = (len(shared_words_in_q1) + len(shared_words_in_q2))/(len(q1words) + len(q2words))
+    return R
